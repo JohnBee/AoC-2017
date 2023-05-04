8176276d3a8c3c553e4f19881743dada52546464
John Bee <john.barra@hotmail.co.uk>
2023-05-04 12:07:12 +0100

    Initial commit

---
+++ template.py
@@ -0,0 +1,17 @@
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def load_file(filename):
+    lines = []
+    with open(filename) as f:
+        while f.
+        lines.append()
+
+if __name__ == "__main__":
+    print("Part 1: " + part1())
+    print("Part 2: " + part2())
