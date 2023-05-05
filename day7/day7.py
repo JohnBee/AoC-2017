001dab253e7ac108cd9209d5c57a475f99e8b181
John Bee <john.barra@hotmail.co.uk>
2023-05-05 15:59:40 +0100

    day 7 init

---
+++ day7/day7.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append('../aochelpers.py')
+from aochelpers import *
+
+
+def part1(input_str: list[str]) -> int:
+    pass
+
+
+def part2(input_str: list[str]) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    lines = load_file("input")
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
